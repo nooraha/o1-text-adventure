458ec1a593b88021235bd3c1c217d4e614c3eceb
noora ha <noora.halla.aho@gmail.com>
2025-11-17 13:30:26 +0200

    add area descriptions txt file

--- o1/adventure/InteractableThingy.scala
+++ o1/adventure/InteractableThingy.scala
@@ -1,7 +1,13 @@
 package o1.adventure
 
 class InteractableThingy(val name: String, val description: String) {
+
+  private val actions = List[String]()
+
   override def toString = this.name
+
+  def addAction(name: String): Int =
+    1+1
 
   // define options for actions when examining this
   // how it should work:
@@ -9,4 +15,12 @@ class InteractableThingy(val name: String, val description: String) {
   // then it shows like a description of the cupboard idk the cupboard contains... something
   // options for player: take gun (idk), return
   // sooo thingy can contain items but can also have its own actions
+
+  // player calls action
+  // if action is a defined action then
+  // run that action with modifiers
+  // have a list of available actions at each moment
+  // list is affected by current area/object
+  // when examining thingy, list of actions is defined completely by that thingy
+  // each thingy should have 'return' action to go back to area
 }
